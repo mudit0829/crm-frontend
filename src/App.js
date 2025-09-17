0bf65935170e8e1b3b0fab867e6ad882a0d8f713
mudit0829 <mekasutechnology@gmail.com>
2025-09-17 10:16:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dashboard from './components/dashboard/Dashboard.jsx';
 function App() {
   return (
     <div>
-      <h1>Welcome to CRM</h1>
+      <h1>CRM Frontend</h1>
       <Login />
       <Dashboard />
     </div>
