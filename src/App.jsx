c805e84366570950c10b36ce2306b57e97f56a01
mudit0829 <mekasutechnology@gmail.com>
2025-09-18 10:09:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './components/auth/Login.jsx';
 import Dashboard from './components/dashboard/Dashboard.jsx';
 
 function App() {
+  const [token, setToken] = useState('');
+
   return (
     <div>
       <h1>CRM Frontend</h1>
-      <Login />
-      <Dashboard />
+      {!token ? (
+        <Login onLogin={setToken} />
+      ) : (
+        <Dashboard token={token} />
+      )}
     </div>
   );
 }
-
 export default App;
